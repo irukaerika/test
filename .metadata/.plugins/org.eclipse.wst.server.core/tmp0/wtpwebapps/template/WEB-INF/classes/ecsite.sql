121398f69cfad720b359c299ea4318755d82ca40
irukaerika <irukaerika5942@gmail.com>
2018-08-03 17:59:26 +0900

    ecsite2

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/template/WEB-INF/classes/ecsite.sql
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/template/WEB-INF/classes/ecsite.sql
@@ -32,8 +32,8 @@ drop table if exists user_buy_item_transaction;
 create table user_buy_item_transaction(
 id int not null primary key auto_increment,
 item_transaction_id int,
-t
-total_count int,otal_price int,
+total_price int,
+total_count int,
 user_master_id varchar(16),
 pay varchar(30),
 insert_date datetime,
