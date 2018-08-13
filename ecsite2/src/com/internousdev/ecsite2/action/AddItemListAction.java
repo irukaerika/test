0bffff128e4025fbd1feb6b427920a8273e2f1c6
irukaerika <irukaerika5942@gmail.com>
2018-08-13 17:56:24 +0900

    8/13

---
+++ ecsite2/src/com/internousdev/ecsite2/action/AddItemListAction.java
@@ -0,0 +1,9 @@
+package com.internousdev.ecsite2.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+public class AddItemListAction extends ActionSupport{
+	public String execute(){
+		return SUCCESS;
+	}
+
+}
